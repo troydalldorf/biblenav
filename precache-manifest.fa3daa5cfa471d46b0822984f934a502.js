f78d6b9ca2c80a32ede6ccf26f2ab827c82b8d42
Troy Dalldorf <tdalldorf@landolakes.com>
2021-03-21 07:28:33 -0500

    Updates

---
+++ precache-manifest.fa3daa5cfa471d46b0822984f934a502.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8fd794ad329a7a6945b6b96eca80c90",
+    "url": "/biblenav/index.html"
+  },
+  {
+    "revision": "72e6776309b0ab95656a",
+    "url": "/biblenav/static/js/2.6e2e59eb.chunk.js"
+  },
+  {
+    "revision": "818cc8b2a0e00c2e5bca",
+    "url": "/biblenav/static/js/main.44c06654.chunk.js"
+  },
+  {
+    "revision": "c392bb814385186ab751",
+    "url": "/biblenav/static/js/runtime~main.30163b8a.js"
+  }
+]);
